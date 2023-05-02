3ba60583d0fb38fe5b44c194e102561b41551836
Rakesh192211848 <132035979+Rakesh192211848@users.noreply.github.com>
2023-05-02 10:39:16 +0530

    Create Question 3.C

---
+++ Question 3.C
@@ -0,0 +1,3 @@
+n = int(input("Enter the value of n: ")) # get user input for value of n
+for i in range(1, n+1, 2): # use a for loop to iterate from 1 to n with a step of 2
+    print(i, end=" ") # print each odd number separated by a space
