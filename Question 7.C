c1c0788270554d2bc957b2f3ae6867ff6a0da6b1
Rakesh192211848 <132035979+Rakesh192211848@users.noreply.github.com>
2023-05-02 10:53:25 +0530

    Create Question 7.C

---
+++ Question 7.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int n, i, sum=0;
+   
+   printf("Enter the number of terms: ");
+   scanf("%d", &n);
+
+   for(i=1; i<=n; i++) {
+      if(i%2 != 0) {
+         sum += i;
+      }
+   }
+
+   printf("The sum of first %d odd numbers is %d.", n, sum);
+
+   return 0;
+}
