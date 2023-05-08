59dccafd49c7adc25f2c3e654baa2343bb68ca59
Rakesh192211848 <132035979+Rakesh192211848@users.noreply.github.com>
2023-05-08 10:31:58 +0530

    Create Question 30.C

---
+++ Question 30.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; // Return the index where the key is found
+        }
+    }
+    return -1; // Return -1 if the key is not found
+}
+
+int main() {
+    int arr[] = {5, 8, 2, 16, 9, 13, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 9;
+
+    int index = linearSearch(arr, n, key);
+
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", key, index);
+    } else {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
